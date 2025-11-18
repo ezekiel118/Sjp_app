895cb484e4212b8e8dc9f59e24eede8993cf002f
ezekiel118 <ramirezjohnezekiel118Gmail.com>
2025-11-18 21:45:29 +0800

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.android.application) // keeps the plugin from your version catalog
+    id("com.google.gms.google-services")    // for Firebase
+}
+
+android {
+    namespace = "com.example.sjp_app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.sjp_app"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+}
